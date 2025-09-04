0a31388b8de927d09e904ee3b38f1cd28d81948f
Kevin Gonzaga <keviin.gonzaga@gmail.com>
2025-09-03 19:07:53 -0500

    modificacion den duplicateExeption

--- src/main/java/com/kgonzaga/note/app/exception/DuplicateResourceException.java
+++ src/main/java/com/kgonzaga/note/app/exception/DuplicateResourceException.java
@@ -2,6 +2,6 @@ package com.kgonzaga.note.app.exception;
 
 public class DuplicateResourceException extends RuntimeException {
     public DuplicateResourceException(String dni, String username) {
-        super("Dni already exists" + dni + " or Username already exists " + username);
+        super("Dni already exists " + dni + " or Username already exists " + username);
     }
 }
